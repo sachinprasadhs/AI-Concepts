5aa5f515cdc5b603a650f04a42600eedd113c143
sineeli <113718461+sineeli@users.noreply.github.com>
2024-03-17 23:10:58 -0700

    Update decoder_transformer.py

--- decoder_transformer.py
+++ decoder_transformer.py
@@ -1,14 +1,16 @@
 import jax
 import optax
-import jax.numpy as jnp
 import numpy as np
+import jax.numpy as jnp
+import flax.linen as nn
+import functools
 
-from typing import Any, Callable
+import flax
+
 from flax import struct
-from flax import linen as nn
-from flax.training import train_state
-from flax.training import dynamic_scale as dynamic_scale_lib
+from typing import Callable, Any
 from flax.training import common_utils
+from flax.training.train_state import TrainState
 
 
 # hyperparameters
@@ -24,8 +26,8 @@ dropout = 0.2
 eval_interval = 500
 
 
- # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('/kaggle/working/input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
@@ -43,45 +45,15 @@ n = int(0.9*len(data)) # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
 
-class TrainState(train_state.TrainState):
-  dynamic_scale: dynamic_scale_lib.DynamicScale
-
-
-
-# data loading
-def get_batch(key, split):
-    # generate a small batch of data of inputs x and targets y
-    key, subkey = jax.random.split(key)
-    data = train_data if split == 'train' else val_data
     
-    ix = jax.random.randint(subkey, minval=0, maxval=(len(data) - block_size), shape=(batch_size,))
-    x = jnp.stack([data[i:i+block_size] for i in ix])
-    y = jnp.stack([data[i+1:i+block_size+1] for i in ix])
-    return x, y
-    
-
-
-def positional_encoding(length=2048, depth=512):
-    depth = depth / 2
-
-    positions = jnp.arange(length)[:, jnp.newaxis]  # (seq, 1)
-    depths = jnp.arange(depth)[jnp.newaxis, :] / depth  # (1, depth)
-
-    angle_rates = 1 / (10000**depths)  # (1, depth)
-    angle_rads = positions * angle_rates  # (pos, depth)
-
-    pos_encoding = jnp.concatenate([jnp.sin(angle_rads), jnp.cos(angle_rads)], axis=-1)
-
-    return jnp.array(pos_encoding, dtype=jnp.float32)
-
-
 @struct.dataclass
 class TransformerConfig:
     """Global hyperparameters"""
-
+    
     vocab_size: int
     # Pre-calculate the positional encoding
     pos_encode: Callable
+    seed: int = 42
     d_model: int = 512
     dropout_rate: float = 0.1
     max_len: int = 2048
@@ -96,8 +68,22 @@ class TransformerConfig:
     bias_init: Callable = nn.initializers.normal(stddev=1e-6)
     learning_rate: float = 3e-4
     max_iters: int = 5000
-    momentum: int = 0.9
+    momentum: float = 0.9
+    per_device_batch_size: int = 64
+    max_target_length: int = 256
+    
+def positional_encoding(length=2048, depth=512):
+    depth = depth / 2
 
+    positions = jnp.arange(length)[:, jnp.newaxis]  # (seq, 1)
+    depths = jnp.arange(depth)[jnp.newaxis, :] / depth  # (1, depth)
+
+    angle_rates = 1 / (10000**depths)  # (1, depth)
+    angle_rads = positions * angle_rates  # (pos, depth)
+
+    pos_encoding = jnp.concatenate([jnp.sin(angle_rads), jnp.cos(angle_rads)], axis=-1)
+
+    return jnp.array(pos_encoding, dtype=jnp.float32)
 
 class TextEmbed(nn.Module):
     config: TransformerConfig
@@ -142,9 +128,11 @@ class PositionalEmbedding(nn.Module):
         x = x + config.pos_encode[jnp.newaxis, :length, :]
         return x
 
-
 class SingleHead(nn.Module):
     config: TransformerConfig
+    
+    def setup(self):
+        self.tril = jnp.tril(jnp.ones((config.block_size, config.block_size)))
 
     @nn.compact
     def __call__(self, inputs):
@@ -168,20 +156,20 @@ class SingleHead(nn.Module):
             kernel_init=config.kernel_init,
             bias_init=config.bias_init,
             use_bias=False)(inputs)  # B, T, C
-
+        
+        length = inputs.shape[1]
         wei = query @ key.transpose((0, 2, 1))  # (B, T, 16) @ (B, 16,  T) -> (B, T, T)
-        tril = jnp.tril(jnp.ones((config.block_size, config.block_size)))
+        tril = self.tril[:length, :length]
         wei = jnp.where(tril == 0, -jnp.inf, wei)
         wei = nn.softmax(wei, axis=-1)
-        wei = nn.Dropout(rate=config.dropout_rate, 
-                         deterministic=config.deterministic)(wei)
+        wei = nn.Dropout(rate=config.dropout_rate)(
+            wei, deterministic=config.deterministic)
         out = wei @ value
         return out
 
 
 class MultiHeadAttention(nn.Module):
     config: TransformerConfig
-    num_layers: int = 4
 
     @nn.compact
     def __call__(self, inputs):
@@ -196,12 +184,9 @@ class MultiHeadAttention(nn.Module):
             kernel_init=config.kernel_init,
             bias_init=config.bias_init,)(x)
         x = nn.Dropout(
-            config.dropout_rate,
-            deterministic=config.deterministic
-        )(x)
+            config.dropout_rate)(x, deterministic=config.deterministic)
 
         return x
-
 
 class MlpBlock(nn.Module):
     config: TransformerConfig
@@ -219,7 +204,6 @@ class MlpBlock(nn.Module):
             inputs
         )  # d_model -> mlp_dim
         x = nn.relu(x)
-
         x = nn.Dense(
             config.d_model,
             dtype=config.dtype,
@@ -231,12 +215,10 @@ class MlpBlock(nn.Module):
         
         x = nn.Dropout(
             config.dropout_rate,
-            deterministic=config.deterministic
-        )(x)
+        )(x, deterministic=config.deterministic)
 
         return x
-
-
+        
 class DecoderBlock(nn.Module):
     """Transformer Decoder block: communication followed by computation"""
 
@@ -253,7 +235,6 @@ class DecoderBlock(nn.Module):
         logits = sa_out + MlpBlock(config)(mlp_in)
 
         return logits
-
 
 class Decoder(nn.Module):
     config: TransformerConfig
@@ -274,85 +255,91 @@ class Decoder(nn.Module):
             config.vocab_size,
             dtype=config.dtype,
             kernel_init=config.kernel_init,
-            bias_init=config.bias_init,)(x)
+            bias_init=config.bias_init)(x)
 
         return lm_head_out
 
+def compute_cross_entropy(logits, labels):
+    vocab_size = logits.shape[-1]
+    targets = common_utils.onehot(labels, vocab_size)
+    loss = -jnp.sum(targets * nn.log_softmax(logits), axis=-1)
+    return loss.sum()
 
-def create_train_state(key, config: TransformerConfig):
-    decoder = Decoder(config, name="decoder_transformer")
-    initial_variables = jax.jit(decoder.init)(key, batched_data[0])
-    adam_opt = optax.adam(config.learning_rate, config.momentum)
-    return TrainState.create(apply_fn=decoder.apply, 
-                             params=initial_variables['params'], 
-                             dynamic_scale=None,
-                             tx=adam_opt)
+def compute_accuracy(logits, targets, weights=None):
+    """Compute weighted accuracy for log probs and targets.
 
+    Args:
+    logits: [batch, length, num_classes] float array.
+    targets: categorical targets [batch, length] int array.
+    weights: None or array of shape [batch, length]
 
-def compute_weighted_accuracy(logits, targets):
+    Returns:
+    Tuple of scalar loss and batch normalizing factor.
+    """
     if logits.ndim != targets.ndim + 1:
         raise ValueError(
             "Incorrect shapes. Got shape %s logits and %s targets"
             % (str(logits.shape), str(targets.shape))
         )
-    loss = jnp.equal(jnp.argmax(logits, axis=-1), targets)
+    acc = jnp.equal(jnp.argmax(logits, axis=-1), targets)
 
-    return loss.sum()
+    return acc.sum()
 
-def compute_loss(logits, labels):
-    vocab_size = logits.shape[-1]
-    targets = common_utils.onehot(labels, vocab_size)
-    loss = -jnp.sum(targets * nn.log_softmax(logits), axis=-1)
-    
-    return loss.sum()
+def compute_metrics(logits, labels):
+    """Compute summary metrics."""
+    loss = compute_cross_entropy(
+        logits, labels
+    )
+    acc = compute_accuracy(logits, labels)
+    metrics = {
+        "loss": loss,
+        "accuracy": acc,
+    }
+    metrics = jax.lax.psum(metrics, axis_name="batch")
+    return metrics
 
-def compute_metrics(*, logits, labels):
-  loss = compute_loss(logits, labels)
-  accuracy = jnp.mean(jnp.argmax(logits, -1) == labels)
-
-  metrics = {
-      'loss': loss,
-      'accuracy': accuracy,
-  }
-  return metrics
-
-
-# @jax.jit
-def train_step(
-    config: TransformerConfig, 
-    state: TrainState, 
-    dropout_key,
-    batch_data):
-    
-    
-    inputs, labels = batch_data
-    dropout_train_key = jax.random.fold_in(key=dropout_key, data=state.step)
-    
+def train_step(state, batch, config, dropout_rng=None):
+    inputs, targets = batch
+    dropout_train_key = jax.random.fold_in(key=dropout_rng, data=state.step)
     def loss_fn(params):
-        logits = state.apply_fn(
-            {'params': params}, 
+        logits = Decoder(config).apply(
+            {"params": params},
             inputs,
-            rngs={'dropout': dropout_train_key})
-        print(logits.shape)
-        loss = compute_loss(logits, labels)
+            rngs={"dropout": dropout_train_key}
+        )
+        
+        loss = compute_cross_entropy(logits, targets)
         return loss, logits
+        
+    (loss, logits), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+    grads = jax.lax.psum(grads, axis_name='batch')
+    new_state = state.apply_gradients(grads=grads)
+    print(logits.shape, targets.shape)
+    metrics = compute_metrics(logits, targets)
+    return metrics, new_state
 
-    (_, logits), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
-    state = state.apply_gradients(grads=grads)
-    metrics = compute_metrics(logits=logits, labels=labels)
-    return state, metrics
+def create_train_state(init_keys, config: TransformerConfig):
+    decoder = Decoder(config)
+    input_shape =  (config.per_device_batch_size, config.max_target_length)
+    initial_variables = jax.jit(decoder.init)(
+        init_keys['params'], 
+        jnp.ones(input_shape, jnp.int32),)
+    return TrainState.create(apply_fn=decoder.apply, 
+                             params=initial_variables['params'], 
+                             tx=optax.adam(config.learning_rate, config.momentum))
 
-# @jax.jit
-def eval_step(
-    config: TransformerConfig, 
-    state: TrainState , 
-    inputs: jnp.ndarray, 
-    labels: jnp.ndarray):
-  logits = Decoder(config).apply({'params': state.params}, inputs)
-  return compute_metrics(logits=logits, labels=labels)
+# data loading
+def get_batch(key, split):
+    # generate a small batch of data of inputs x and targets y
+    key, subkey = jax.random.split(key)
+    data = train_data if split == 'train' else val_data
     
+    ix = jax.random.randint(subkey, minval=0, maxval=(len(data) - block_size), shape=(batch_size,))
+    x = jnp.stack([data[i:i+block_size] for i in ix])
+    y = jnp.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y, key
 
-
+# declare config object
 config = TransformerConfig(
     vocab_size=vocab_size,
     pos_encode=positional_encoding(depth=n_embed),
@@ -364,27 +351,74 @@ config = TransformerConfig(
     num_layers=n_layer,
     mlp_dim=n_embed * 4,
     learning_rate=3e-4,
-    max_iters=5000
+    max_iters=5000,
+    deterministic=True, # Basically training is False
+    kernel_init=nn.initializers.xavier_uniform(),
+    bias_init=nn.initializers.normal(stddev=1e-6)
 )
 
-data_key, params_key, dropout_key, opt_key = jax.random.split(jax.random.PRNGKey(seed), 4)
-batched_data = get_batch(data_key, 'train')
+# Generate Initial Keys
+rng = jax.random.key(config.seed)
+rng, init_rng = jax.random.split(rng)
 
-inti_keys = {'params': params_key, 'dropout': dropout_key}
-state = create_train_state(inti_keys, config)
+state = create_train_state(
+    {'params': init_rng},
+    config
+)
 
+config = config.replace(deterministic=False)
 
-for iter in range(config.max_iters):
-    data_key, data_subkey = jax.random.split(data_key)
-    batch_data = get_batch(data_key, 'train')
-    # print(inputs.dtype, labels.shape, train_state)
-    state, metrics = train_step(config, state, dropout_key, batch_data)
+state = flax.jax_utils.replicate(state)
+dropout_rngs = jax.random.split(rng, jax.local_device_count())
+
+p_train_step = jax.pmap(
+    functools.partial(
+        train_step,
+        config=config,
+    ),
+    axis_name="batch",
+    donate_argnums=(0,),
+)
+
+print(config)
+
+for step in range(config.max_iters):
+    inputs, targets, rng = get_batch(rng, 'train') 
+    # Shard data to devices and do a training step.
+    with jax.profiler.StepTraceAnnotation("train", step_num=step):
+        inputs = common_utils.shard(
+            jax.tree_util.tree_map(np.asarray, inputs)
+        )
+        targets = common_utils.shard(
+            jax.tree_util.tree_map(np.asarray, targets)
+        )
+        metrics, state = p_train_step(state, (inputs, targets), dropout_rng=dropout_rngs)
+    if step % 10 == 0:
+        print(metrics)
+
+def generate(key, config: TransformerConfig, params, idx, max_new_tokens):
+    for _ in range(max_new_tokens):
+        key, sub_key = jax.random.split(key)
+        idx_cond = idx[:, -config.block_size:]
+        logits = Decoder(config).apply(
+            {"params": params},
+            idx_cond
+        )
+        # focus only on the last time step
+        logits = logits[:, -1, :] # becomes (B, C)
+        # apply softmax to get probabilities
+        probs = nn.softmax(logits, axis=-1) # (B, C)
+        # sample from the distribution
+        idx_next = jax.random.categorical(sub_key, probs, axis=-1) # (B, 1)
+        idx_next = jnp.expand_dims(idx_next, axis=1)
+        # append sampled index to the running sequence
+        idx = jnp.concatenate((idx, idx_next), axis=1) # (B, T+1)
     
-    if iter % eval_interval == 0 or iter == config.max_iters - 1:
-        print(f"Step: {iter}, training loss: {metrics['loss']}, training accuracy: {metrics['accuracy']}")
-        eval_inputs, eval_labels = get_batch(data_subkey, 'val')
-        val_metrics = eval_step(config, state, eval_inputs, eval_labels)
-        print(f"Step: {iter}, validation loss: {val_metrics['loss']}, validation accuracy: {val_metrics['accuracy']}")
-    
-    
-        
+    return idx
+
+context = jnp.zeros((1, 1), dtype=jnp.int32)
+config = config.replace(deterministic=True)
+deprelicate_state = flax.jax_utils.unreplicate(state)
+tokens = generate(rng, config, deprelicate_state.params, context, max_new_tokens=500)
+
+decode(tokens.tolist()[0])
